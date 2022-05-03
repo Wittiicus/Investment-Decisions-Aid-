dbb923a9726854d8b1b5f94b0f3c9bfd17e0b831
Wittiicus <adrixdewit@gmail.com>
2022-05-03 10:08:40 -0400

    Added Json response function and file upload system

--- mainLoanBackend/firstPage/views.py
+++ mainLoanBackend/firstPage/views.py
@@ -1,5 +1,8 @@
+import json
+import pandas as pd 
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 import joblib
@@ -7,7 +10,22 @@ import joblib
 model = joblib.load('modelPipeline.pkl')
 
 def takeJson(formRequest):
+    print (formRequest.body)
+    data = json.loads(formRequest.body)
+    dataFrm = pd.DataFrame({'x': data}).transpose()
+    score = model.predict_proba(dataFrm)[:, -1][0]
+    score = float(score)
     return JsonResponse({'score': 1})
 
 def takeFile(fileRequest):
+    fileObject = fileRequest.FILES['filePath']
+    fs = FileSystemStorage() 
+    filePathName = fs.save(fileObject.name, fileObject)
+    filePathName = fs.url(filePathName)
+    filePath = '.' + filePathName
+
+    data = pd.read_csv(filePath)
+    score = model.predict_proba(dataFrm)[:,-1]
+
+    score = {j:k for j,k in zip(data['Loan ID'], score)}
     return JsonResponse({'score': 1})
